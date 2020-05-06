b0a964ffb3cc0fd7b76638af90fe7bab721ffb54
Emerson Gonzalez <emersonjavid97@gmail.com>
2020-05-06 11:27:50 -0500

    practice of structure

---
+++ MyPlayground.playground/Contents.swift
@@ -0,0 +1,77 @@
+import UIKit
+
+var str: String = "Hello, playground..."
+
+let myName: String = "Emerson"
+let myLastName: String = "Morales"
+let myFullName: String = "\(myName) Gonzalez \(myLastName)"
+
+var mySalary: Int = 0
+mySalary = 1
+
+func calculateBMI(weight: Double, height: Double) -> Double {
+    return weight / pow(height, 2)
+}
+
+func loop(from: Int, to: Int) {
+    print("Start loop::::")
+    for number in from..<to where number % 2 == 0 {
+        print(number)
+    }
+}
+
+func songBeer(from: Int, to: Int) -> String {
+    print("Start song::::")
+    var lyrics: String = "";
+    for index in  stride(from: from, through: to, by: -1) {
+        lyrics += "\n \(index) bottles of beer on the wall, \(index) bottles of beer. \n Take one down and pass it around, \((index - 1)) bottles of beer on the wall. \n"
+    }
+    return lyrics
+}
+
+func songBeerNew(from: Int, to: Int) -> String {
+    print("Start song::::")
+    var lyrics: String = "";
+    for index in  (from...to).reversed() {
+        lyrics += "\n \(index) bottles of beer on the wall, \(index) bottles of beer. \n Take one down and pass it around, \((index - 1)) bottles of beer on the wall. \n"
+    }
+    return lyrics
+}
+
+func fibonachi(until: Int){
+    print("Fibonachi::::")
+    var start: Int = -1
+    var startTwo: Int = 1
+    for _ in 0...until {
+        let number =  (start + startTwo)
+        start = startTwo
+        startTwo = number
+        print(number)
+    }
+}
+
+fibonachi(until: 10)
+
+print(songBeerNew(from: 1, to: 10))
+
+loop(from: 1, to: 20)
+
+print(calculateBMI(weight: 76.5 , height: 1.70))
+
+//**************STRUCTURES**********************
+
+struct MyTown {
+    let name = "Planeta Rica"
+    var citizens = ["Emerson", "Tatiana", "Mónica"]
+    var resources = ["Grain": 100, "Ore": 420, "wool": 500]
+    
+    func fortity() {
+        print("Defences increased!")
+    }
+}
+
+var myTown = MyTown()
+
+print(myTown.citizens)
+print("\(myTown.name) has \(myTown.resources["Grain"]!) bags of grains")
+myTown.fortity()
